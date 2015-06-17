c2037fa345158d08ce70a95efd81058ceff918ad
Pedro Pacheco <pedropacheco@ptisp.pt>
2015-06-17 15:30:05 +0100

    fixes

---
+++ examples/licenses.js
@@ -0,0 +1,17 @@
+var CLINUX = require('../lib/cloudlinux');
+
+var config = {
+  user: process.env.CLINUX_USER,
+  token: process.env.CLINUX_TOKEN
+};
+
+var clexample = new CLINUX(config);
+
+clexample.keyListKC( function(err, data){
+  if (err) {
+    console.log('ERROR');
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
